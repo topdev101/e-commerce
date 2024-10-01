ff39702f4ffb31a09d6c43fa4dbe8b91e8431c5d
topdev101 <ritachi0502@gmail.com>
2024-10-01 09:21:12 -0400

    initial project

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"], // Paths to all your template files
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {}, // Extend or customize your theme here
+  },
+  plugins: [],
+};
